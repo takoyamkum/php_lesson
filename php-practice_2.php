fb8505b0fa60f812c03dbd618fcc84e5c334c32c
takoyamkum <122874597+takoyamkum@users.noreply.github.com>
2024-04-26 18:01:09 +0900

    thirdcommit

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,28 +1,9 @@
 <?php
 // Q1 tic-tac問題
-for ($i = 1; $i <= 100; $i++) {
-  if ($i % 20 === 0) {
-      echo 'tic-tac';
-      echo "\n";
-  } elseif ($i % 4 === 0) {
-      echo 'tic';
-      echo "\n";
-  } elseif ($i % 5 === 0) {
-      echo 'tac';
-      echo "\n";
-  } else {
-      echo $i;
-      echo "\n";
-  }
-}
+
 
 // Q2 多次元連想配列
 
-//問題１
-echo '' .$personalInfos[1]['name']. 'の電話番号は' 
-.$personalInfos[1]['tel']. 'です。';
-
-//問題２
 
 // Q3 オブジェクト-1
 
